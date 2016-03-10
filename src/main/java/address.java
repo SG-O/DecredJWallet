c190ef1b02b6fb80d93edce62fb50cbef395d589
SG-O <sg-o@live.de>
2016-03-10 03:18:58 +0100

    Added has hash and equals as well as some getters

--- src/main/java/address.java
+++ src/main/java/address.java
@@ -21,6 +21,10 @@ public class address {
     }
 
     public address(String address, int netIdentifierLength) {
+        if (address == null) return;
+        if (address.equals("")) return;
+        address = address.trim();
+        if (!(address.length() > (netIdentifierLength + 4))) return;
         byte[] addressHex = base58.fromBase58(address);
         HashMap<Integer, byte[]> seperated = hex.hexSeparate(addressHex, netIdentifierLength);
         if (seperated != null) {
@@ -33,8 +37,38 @@ public class address {
         }
     }
 
+    public boolean isEmpty(){
+        if ((publicKeyHash == null) || (shortHash == null) || (networkBytes == null)) return true;
+        return (publicKeyHash.length == 0) || (shortHash.length == 0) || (networkBytes.length == 0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        address address = (address) o;
+
+        if (!Arrays.equals(publicKeyHash, address.publicKeyHash)) return false;
+        if (!Arrays.equals(shortHash, address.shortHash)) return false;
+        return Arrays.equals(networkBytes, address.networkBytes);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(publicKeyHash);
+        result = 31 * result + Arrays.hashCode(shortHash);
+        result = 31 * result + Arrays.hashCode(networkBytes);
+        return result;
+    }
+
+    public String getHashString(){
+        return hex.bytesToHex(calculateShortHash());
+    }
+
     public boolean check() {
-        return Arrays.equals(shortHash, hashWrapper.shortDoubleHashSha256(getKeyWithNet()));
+        return Arrays.equals(shortHash, calculateShortHash());
     }
 
     public byte[] getPublicKeyHash() {
