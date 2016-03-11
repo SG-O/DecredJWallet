535a8c6b1bcfead4285fcade5f6c7574828004f9
SG-O <sg-o@live.de>
2016-03-11 04:32:32 +0100

    Added new Address Book GUI

---
+++ src/main/java/AddressBookGui.java
@@ -0,0 +1,147 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+/**
+ * DecredJWallet: Created by Joerg Bayer(admin@sg-o.de) on 11.03.2016.
+ */
+public class AddressBookGui extends JDialog {
+    private JButton cancelButton;
+    private JButton OKButton;
+    private JTextField commentField;
+    private JTextField addressField;
+    private JTextField nameField;
+    private JButton addButton;
+    private JPanel contentPane;
+    private JPanel book;
+    private JButton removeButton;
+    private JTable table;
+    private JScrollPane tableScroll;
+    private TableModel tableModel;
+
+    private boolean doTransaction;
+    private addressBook adrBook;
+    private settings set;
+
+    public AddressBookGui(addressBook adrBook, settings set, boolean doTransaction) {
+        this.doTransaction = doTransaction;
+        this.set = set;
+        setContentPane(contentPane);
+        setTitle("Address Book");
+        setAlwaysOnTop(true);
+        setModal(true);
+
+        this.adrBook = adrBook;
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+        }
+
+        try {
+            setIconImage(ImageIO.read(ClassLoader.getSystemResource("favicon.png")));
+        } catch (IOException e) {
+        }
+
+        contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        setMinimumSize(new Dimension(550, 400));
+        setSize(550, 400);
+        getRootPane().setDefaultButton(cancelButton);
+
+        OKButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        cancelButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                addAddress();
+            }
+        });
+
+        removeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                removeAddress();
+            }
+        });
+
+// call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+// call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        setLocationRelativeTo(null);
+        generateTable();
+        setVisible(true);
+    }
+
+    private void generateTable() {
+        table = new JTable(new addressBookTableModel());
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tableModel = table.getModel();
+        ((addressBookTableModel) tableModel).changeData(adrBook);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        table.getColumnModel().getColumn(0).setPreferredWidth(30);
+        table.getColumnModel().getColumn(1).setPreferredWidth(110);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+        // Add the table to a scrolling pane
+        tableScroll = new JScrollPane(table);
+        book.add(tableScroll, BorderLayout.CENTER);
+    }
+
+    private void removeAddress() {
+        if (table.getSelectedRow() < 0) return;
+        adrBook.removeEntry(((addressBookTableModel) tableModel).getValueAtRow(table.getSelectedRow()));
+        ((addressBookTableModel) tableModel).changeData(adrBook);
+    }
+
+    private void addAddress() {
+        if (addressField.getText().trim().length() < (decredConstants.getNetConstants(set).getPubKeyHashAddrID().length + 5)) {
+            new Error("Error", "Address is too short.");
+            return;
+        }
+        address temp = new address(addressField.getText().trim(), decredConstants.getNetConstants(set).getPubKeyHashAddrID().length);
+        if (!temp.check()) {
+            new Error("Error", "Invalid address: " + temp);
+            return;
+        }
+        String nameText = nameField.getText().trim();
+        if (nameText.length() == 0) {
+            nameText = temp.toString();
+        }
+        adrBook.addEntry(new addressBookEntry(temp, nameText, commentField.getText().trim()));
+        ((addressBookTableModel) tableModel).changeData(adrBook);
+    }
+
+    private void onCancel() {
+        dispose();
+    }
+
+    private void onOK() {
+        adrBook.saveAddressBook();
+        dispose();
+        if (doTransaction && (table.getSelectedRow() >= 0)) {
+            new newTransaction(((addressBookTableModel) tableModel).getValueAtRow(table.getSelectedRow()).getAdr(), new Coin(), set);
+        }
+    }
+}
+
